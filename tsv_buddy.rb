7db03ba878cd7f57887848d4b3379fe0eee4dcbc
u10000129 <u10000129@gmail.com>
2018-10-04 13:40:27 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,30 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    @data = []
+    lines = tsv.split("\n")
+    keys = lines[0].split("\t")
+    lines.shift
 
+    lines.each do |line|
+      values = line.split("\t")
+      record = {}
+      keys.each_index { |index| record[keys[index]] = values[index] }
+      @data.push(record)
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    tsv_str = ''
+    header = @data[0].keys.join("\t")
 
+    tsv_str << header << "\n"
+    @data.each do |record|
+      line = record.values.join("\t")
+      tsv_str << line << "\n"
+    end
+    tsv_str
   end
 end
